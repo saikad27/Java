8e1d307ea038fd390e53c36dbf050db4d7723b75
saikad27 <sairajkad501@icloud.com>
2024-06-16 17:01:41 +0530

    second commit

--- Java/Practicals/Array1_14/P8.java
+++ Java/Practicals/Array1_14/P8.java
@@ -1,19 +1,15 @@
 import java.io.*;
-class P7{
+class P8{
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Enter the size of array : ");
+		System.out.print("Enter total count of employees : ");
 		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
-		System.out.println("Enter elements of array : ");
+		System.out.println("Enter Employee's Age : ");
 		for(int i=0;i<arr.length;i++){
 			arr[i] = Integer.parseInt(br.readLine());
 		}
-		System.out.println("Array elements divisible by 4: ");
-		for(int i=0; i<arr.length;i++){
-			if(arr[i]%4==0)
-				System.out.println(arr[i]+" is divisible by 4 and its index is "+i);
-		}
+		
 	}
 
 }
